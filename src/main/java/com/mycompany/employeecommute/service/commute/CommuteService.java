831c692fd8918b039eb5750c57a05c41078418ad
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-03-08 18:17:02 +0900

    style remove comments not needed

--- src/main/java/com/mycompany/employeecommute/service/commute/CommuteService.java
+++ src/main/java/com/mycompany/employeecommute/service/commute/CommuteService.java
@@ -31,7 +31,6 @@ public class CommuteService {
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(IllegalArgumentException::new);
 
-        //오늘날에 출근을 했다면 에러를 던진다
         if (commuteHistoryRepository.existsByEmployeeAndDate(employee, LocalDate.now())) {
             throw new IllegalArgumentException(String.format("employee(%s)은(는) 이미 출근을 했습니다.", employee.getName()));
         }
@@ -47,7 +46,6 @@ public class CommuteService {
         CommuteHistory commuteHistory = commuteHistoryRepository.findByEmployeeAndDate(employee, LocalDate.now())
                 .orElseThrow(IllegalArgumentException::new);
 
-        //만약 퇴근 이력이 있다면 예외를 던진다
         if (commuteHistory.leavingTime() != null) {
             throw new IllegalArgumentException("퇴근 상태입니다. 출근을 먼저 해주세요.");
         }
@@ -57,26 +55,20 @@ public class CommuteService {
 
     @Transactional
     public CommuteMonthHistoryResponse getCommuteMonthHistory(Long employeeId, YearMonth yearMonth) {
-        //1.직원 정보를 가져온다.
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(IllegalArgumentException::new);
 
-        //년월 -> 그 월에 해당하는 모든 일(day)
         LocalDate firstOfMonth = yearMonth.atDay(1);
         LocalDate endOfMonth = yearMonth.atEndOfMonth();
 
-        //2.요청한 년월에 해당하는 해당 직원의 근무이력들을 가져온다.
+        //요청한 년월(yearMonth)에 해당하는 해당 직원의 근무이력들을 가져온다.
         List<CommuteHistory> historiesOfMonth = commuteHistoryRepository
                 .findByEmployeeAndDateBetween(employee, firstOfMonth, endOfMonth);
 
-        //3.근무이력들에서 date를 가져와 리스트에 담는다.
-        //4.근무일에서 퇴근 시간과 출근 시간의 차이를 분으로 환산한다.
         List<Detail> details = getDetails(historiesOfMonth); // todo: 퇴근을 안한 상태로 getCommuteMonthHistory를 호출하면 N.P.E가 발생한다.
 
-        //5.근무 시간을 모두 더한다.
         long sum = getSum(details);
 
-        //6.CommuteMonthHistoryResponse 객체에 담는다.
         return new CommuteMonthHistoryResponse(details, sum);
     }
 
@@ -92,6 +84,8 @@ public class CommuteService {
         if (history.leavingTime() == null) {
             return new Detail(history.getDate(), 0, true);
         }
+
+        //근무일에서 퇴근 시간과 출근 시간의 차이를 분으로 환산한다.
         return new Detail(history.getDate(),
                 Duration.between(history.arrivingTime(), history.leavingTime()).toMinutes(),
                 false);
